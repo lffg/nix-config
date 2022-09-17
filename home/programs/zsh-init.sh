eec4df5a696282810dbac229901a97ee800148d0
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-09-17 20:59:15 -0300

    fix(zsh): add `fix_home_manager_path_priority`

--- home/programs/zsh-init.sh
+++ home/programs/zsh-init.sh
@@ -3,6 +3,24 @@ if [ -e ~/.env.sh ]; then
   . ~/.env.sh
 fi
 
+fix_home_manager_path_priority() {
+  local flake_path=""
+  local rest_path=""
+
+  IFS=':' read -r -A path_entries <<< "$PATH"
+  for entry in "${path_entries[@]}"; do
+    if [[ $entry == "/nix/store/"* ]]; then
+      flake_path+="${flake_path:+:}$entry"
+    else
+      rest_path+="${rest_path:+:}$entry"
+    fi
+  done
+
+  export PATH="$flake_path:$rest_path"
+}
+
+[ -n "$IN_NIX_SHELL" ] && fix_home_manager_path_priority
+
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 
 export DIRENV_LOG_FORMAT=''
