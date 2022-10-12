aab22c6fcc7859f1526a04ce8536e9b156a3bed7
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2022-10-12 09:49:31 -0300

    feat: add `rw`

--- home/programs/zsh-fns.sh
+++ home/programs/zsh-fns.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 gcp-c() {
     local CONFIG="${1:-default}"
     gcloud config configurations activate "$CONFIG"
@@ -8,7 +10,15 @@ gcp() {
     local ARGS=("${@:2}")
 
     case "$SUB_COMMAND" in
-        c) gcp-c $ARGS ;;
+        c) gcp-c "${ARGS[@]}" ;;
         *) >&2 echo "error: $SUB_COMMAND: invalid sub command" ;;
     esac
+}
+
+rw() {
+    if ! first=$(which "$1"); then
+        >&2 echo "$first"
+        return 1
+    fi
+    realpath "$first"
 }
